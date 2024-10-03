89691fde15b20279242d774abb3e513cd8ee1360
Taskeen <taskeen.fa@example.com>
2024-10-03 21:27:13 +0530

    new commit

---
+++ src/components/random-color/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Random=()=>{
+  return (
+    <>
+    <div className="container">
+        <button>Generate Random Color</button>
+    </div>
+    </>
+  )
+}
+
+export default Random;
