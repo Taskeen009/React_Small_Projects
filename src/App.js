ddb026a61a596a68026aa9d222a76d2fa7288580
Taskeen <taskeen.fa@example.com>
2024-10-04 22:55:41 +0530

    Random color component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Accordian from './components';
 import './components/style.css';
-
+import './components/random-color'
+import Random from './components/random-color';
 function App() {
   return (
     <div className="App">
-      <Accordian/>
+      {/* <Accordian/> */}
+
+      {/* Random color component */}
+      <Random/>
     </div>
   );
 }
--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Random=()=>{
+  const [typeofcolour, setTypepfcolour]=useState('hex')
+  const [color,setColor]=useState('#000000')
+
+  const handleCreateRandomHexColor=()=>{
+    const hex=[1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+    
+  }
+
+  const handleCreateRandomRgbColor=()=>{
+
+  }
   return (
     <>
-    <div className="container">
-        <button>Generate Random Color</button>
+    <div style={{
+      width:'100vw',
+      height:'100vh',
+      background:color
+    }}>
+        <button onClick={()=>setTypepfcolour('hex')}>Generate Hex Color</button>
+        <button onClick={()=>setTypepfcolour('Rgb')}>Create RGB colour</button>
+        <button onClick={typeofcolour==='hex'? handleCreateRandomHexColor: handleCreateRandomRgbColor}>Genrate Random Color</button>
     </div>
     </>
   )
